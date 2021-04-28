6e6dfab706b40ce910de04d3f0ca5ecd65930cb2
Tomas <norkunas.tom@gmail.com>
2021-04-28 06:56:50 +0300

    Add missing errorNames to Recaptcha3 validator constraint

--- Validator/Constraints/Recaptcha3.php
+++ Validator/Constraints/Recaptcha3.php
@@ -11,6 +11,10 @@ final class Recaptcha3 extends Constraint
 {
     const INVALID_FORMAT_ERROR = '7147ffdb-0af4-4f7a-bd5e-e9dcfa6d7a2d';
 
+    protected static $errorNames = [
+        self::INVALID_FORMAT_ERROR => 'INVALID_FORMAT_ERROR',
+    ];
+
     public $message = 'Your computer or network may be sending automated queries';
     public $messageMissingValue = 'The captcha value is missing';
 }
