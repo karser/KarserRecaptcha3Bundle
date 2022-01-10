5139502293d0d19e73f82bd82df2273fe8c5f9b5
Tomas <norkunas.tom@gmail.com>
2022-01-10 12:04:39 +0200

    Allow using validation constraint as PHP8 attribute

--- Validator/Constraints/Recaptcha3.php
+++ Validator/Constraints/Recaptcha3.php
@@ -6,7 +6,9 @@ use Symfony\Component\Validator\Constraint;
 
 /**
  * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
  */
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
 final class Recaptcha3 extends Constraint
 {
     const INVALID_FORMAT_ERROR = '7147ffdb-0af4-4f7a-bd5e-e9dcfa6d7a2d';
@@ -17,4 +19,12 @@ final class Recaptcha3 extends Constraint
 
     public $message = 'Your computer or network may be sending automated queries';
     public $messageMissingValue = 'The captcha value is missing';
+
+    public function __construct(?array $options = null, ?string $message = null, ?string $messageMissingValue = null, ?array $groups = null, $payload = null)
+    {
+        parent::__construct($options ?? [], $groups, $payload);
+
+        $this->message = $message ?? $this->message;
+        $this->messageMissingValue = $messageMissingValue ?? $this->messageMissingValue;
+    }
 }
